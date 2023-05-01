00edd987f406ab1fb5316ee71aacf13dc59bc34c
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-05-01 17:24:07 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -3,25 +3,35 @@ status2 = "";
 objects = [];
 
 function preload(){
-    Img = loadImage("dog_cat.jpg");
+    
 }
 function setup(){
-    Canvas = createCanvas(640,420);
+    Canvas = createCanvas(380,380);
     Canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+function start(){
     objectDetector = ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting objects";
 }
 function draw(){
-    image(Img,0,0,640,420);
+    image(video,0,0,380,380);
     if(status2 != ""){
+        objectDetector.detect(video,gotposes);
         for(i= 0;i< objects.length;i++){
+            r = random(255);
+            g = random(255);
+            b = random(255);
             document.getElementById("status").innerHTML = "Status: Object detected";
-        fill("red");
+            document.getElementById("amount").innerHTML = "The amount of objects detected are: "+ objects.length;
+        fill(r,g,b);
         textSize(20);
         percent = floor(objects[i].confidence * 100);
         text(objects[i].label+ " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
         noFill();
-        stroke("red");
+        stroke(r,g,b);
         rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height); 
         }
         
@@ -33,7 +43,7 @@ function draw(){
 function modelLoaded(){
     console.log("ModelLoaded");
     status2 = true;
-    objectDetector.detect(Img,gotposes);
+   
 }
 function gotposes(error,results){
     if(error){
